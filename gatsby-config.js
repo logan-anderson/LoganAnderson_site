5ef9a599fcb5cb9adaea1917f32a41b6b673837f
Logan Anderson <landerson2@upei.ca>
2020-04-21 13:58:42 -0300

    added more stuff for SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: `Logan Anderson`,
     description: `The personal website of Logan Anderson. Logan Anderson is a web developer in Prince Edward Island. His main interest's are full-stack development. He addends the University of Prince Edward Island. He can make websites like this one`,
     author: `Logan Anderson`,
+    siteUrl: `https://www.logan.bio`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -29,6 +30,8 @@ module.exports = {
         icon: `src/images/favicon-32x32.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sitemap`,
+    'gatsby-plugin-robots-txt',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
