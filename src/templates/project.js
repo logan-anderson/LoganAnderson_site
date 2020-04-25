90b547eb6fbd268d9ceee19797294cc0a85c3dc5
Logan Anderson <landerson2@upei.ca>
2020-04-25 13:36:00 -0300

    added files

--- src/templates/project.js
+++ src/templates/project.js
@@ -31,8 +31,8 @@ const Template = ({ data }) => {
                 <hr />
 
                 <ReactMarkdown source={post.rawMarkdownBody}> 
-
                 </ReactMarkdown>
+                <ReactMarkdown source={modifiedValues.markdownContent}></ReactMarkdown>
                 <p>Code is available on GitHub <a target="_blank" rel="noopener noreferrer" href={post.frontmatter.gitLink} >here</a> </p>
             </div>
         </Layout>
